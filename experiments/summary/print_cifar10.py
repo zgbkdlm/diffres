781bf9a7b8f6ee89e2b529ddd3e8f6c1b303f4e9
zgbkdlm <zgbkdlm@gmail.com>
2026-03-29 19:45:09 +0200

    add plotter

--- experiments/summary/print_cifar10.py
+++ experiments/summary/print_cifar10.py
@@ -3,6 +3,7 @@ import jax
 import jax.numpy as jnp
 import torch
 import numpy as np
+import matplotlib.pyplot as plt
 from torchmetrics.functional.classification import multiclass_calibration_error, multiclass_f1_score
 from diffres.data import CIFAR10
 from diffres.nns import pbnn_cifar10
@@ -31,13 +32,16 @@ methods = ['diffusion', 'gumbel', 'soft', 'baseline']
 num_mcs = 5
 nparticles = args.nparticles
 
+max_mem = 200  # my computer cannot load the entire test data
+batches_x = np.array_split(dataset.test_xs, max_mem)
+
 for method in methods:
     accs = np.zeros((num_mcs, nparticles))
     eces = np.zeros((num_mcs, nparticles))
     f1s = np.zeros((num_mcs, nparticles))
     log_wss = np.zeros((num_mcs, nparticles))
     for mc_id in range(num_mcs):
-        filename = f'./cifar10/checkpoints/cp-{method}-{mc_id}.npz'
+        filename = f'./cifar10/checkpoints/cp-best-{method}-{mc_id}.npz'
         cp = np.load(filename)
         psi = cp['psi']
         log_ws = cp['log_ws']
@@ -46,15 +50,29 @@ for method in methods:
         # Compute accuracy and ECE
         log_wss[mc_id] = log_ws
         for i in range(nparticles):
-            pred = pbnn_forward_pass(samples[i], psi, dataset.test_xs[:100])
-            accs[mc_id, i] = accuracy(pred, jax.nn.one_hot(dataset.test_ys[:100, 0], 10))
-            f1s[mc_id, i] = multiclass_f1_score(torch.tensor(np.asarray(pred)),
-                                                torch.tensor(np.asarray(dataset.test_ys[:100, 0])),
+            preds = []
+            for batch_x in batches_x:
+                preds.append(pbnn_forward_pass(samples[i], psi, batch_x))
+            preds = jnp.concatenate(preds, axis=0)
+            accs[mc_id, i] = accuracy(preds, jax.nn.one_hot(dataset.test_ys[:, 0], 10))
+            f1s[mc_id, i] = multiclass_f1_score(torch.tensor(np.asarray(preds)),
+                                                torch.tensor(np.asarray(dataset.test_ys[:, 0])),
                                                 num_classes=10)
-            eces[mc_id, i] = multiclass_calibration_error(torch.tensor(np.asarray(pred)),
-                                                          torch.tensor(np.asarray(dataset.test_ys[:100, 0])),
+            eces[mc_id, i] = multiclass_calibration_error(torch.tensor(np.asarray(preds)),
+                                                          torch.tensor(np.asarray(dataset.test_ys[:, 0])),
                                                           num_classes=10, n_bins=15, norm='l1')
 
     print(f'{method} acc', np.mean(np.sum(np.exp(log_wss) * accs, axis=-1)))
     print(f'{method} f1', np.mean(np.sum(np.exp(log_wss) * f1s, axis=-1)))
     print(f'{method} ece', np.mean(np.sum(np.exp(log_wss) * eces, axis=-1)))
+
+# Plot
+for method in methods:
+    for mc_id in range(1):
+        filename = f'./cifar10/checkpoints/log-{method}-{mc_id}.npz'
+        log = np.load(filename)
+        losses_train = log['losses_train']
+        plt.plot(np.log(losses_train), label=f'{method}', alpha=.3)
+        print(f'{method} ESS: {np.mean(log['esss_train'])}')
+plt.legend()
+plt.show()
